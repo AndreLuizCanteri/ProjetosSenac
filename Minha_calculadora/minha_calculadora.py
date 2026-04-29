85ee3f78d7da59ae9ad9c8ee3bd31406d4111e39
Andre Canteri <andre.lcfrutuoso@senacsp.edu.br>
2026-04-28 22:24:10 -0300

    Projeto de calculadora utilizando a POO em python, fazendo uma biblioteca de calculadora

---
+++ Minha_calculadora/minha_calculadora.py
@@ -0,0 +1,12 @@
+def calculadora (a,b):
+    return a + b, a - b, a * b
+
+
+
+def divisao (x,y):
+    if x == 0 or y == 0:
+        print ("O valor digitado nao é possivel dividir")
+    else:
+        return x/y 
+
+
---
+++ Minha_calculadora/teste_calculadora.py
@@ -0,0 +1,4 @@
+import minha_calculadora
+
+print(minha_calculadora.calculadora(10,5))
+print(minha_calculadora.divisao(9,3))
